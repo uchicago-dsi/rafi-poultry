d72cad56bd9b696e1bf336c8e9613c961bfa5bbc
stevejchacon <118153938+stevejchacon@users.noreply.github.com>
2023-07-17 11:29:14 -0500

    add docstring to search_sic_code
    Former-commit-id: 8e915b6aa0424e1e48540a183a5210a76e59d649

--- pipeline/search_sic_code.py
+++ pipeline/search_sic_code.py
@@ -2,17 +2,32 @@ import dask.dataframe as dd
 
 
 def to_dataframe(filepath):
+    """
+        creates dataframe from a file
+        
+        Args:
+            filepath (string): filepath of desired file
+        
+        Returns:
+            dataframe in Dask format
+    """
     df = dd.read_csv(filepath, dtype=str, encoding='unicode_escape')
     df.columns = df.columns.str.upper() # force all columns to be uppercase
     return df
 
 
-# function to filter the DataFrame that has everything read in from the master CSV.
-# Inputs are the master dataframe and the SIC code of choice. Returns a Dask DataFrame
-# that has been filtered to be the lines from Infogroup records that contain the desired
-# SIC Code in 1 of 5 different columns
-
 def sic_matches_df(mst_df, sic_code: str):
+    
+    """
+        located SIC Code patches between a master dataframe and a specific SIC Code
+        
+        Args:
+            mst_df (dataframe): master dataframe from import CSV
+            sic_code (string): desired SIC Code to search for
+            
+        Returns:
+            a dataframe containing only the row that contain the desired SIC Code
+    """
     
     # reassure SIC Code is a string, sanity check    
     sic_code = str(sic_code)
@@ -29,10 +44,3 @@ def sic_matches_df(mst_df, sic_code: str):
     result = filtered_df.compute()
             
     return result
-
-
-## Question: should I create a function to turn it into a csv?
-## a script that can just be ran from top to bottm for the entire
-## process?
-## name of the file? filepath? etc...
-    
