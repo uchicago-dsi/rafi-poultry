efa2a9bb9f0ef5d00c0bbee061e7ff1949399b81
Alena Zeng <azeng@cs.uchicago.edu>
2023-07-24 22:51:08 -0500

    working on main
    Former-commit-id: c8ef23966f90ae53bf48252dc14ef4ea4a2cce13

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,37 +1,51 @@
 import clean
+import match_farms
+import match_plants
+import calculate_captured_areas
 from ..notebooks.utils import visualize
 from ..notebooks.utils import analyze
 import argparse
 from pathlib import Path
+import pandas as pd
 
-def main(data_dir):
-	# try:    
-    	# DATA CLEANING 
-    	# print("Cleaning FSIS data")
-		# clean.clean_fsis(data_dir) # saves cleaned data to the defined output directory
-	# 	print("Cleaning Counterglow")
-	# 	clean.clean_counterglow(data_dir)
-	# 	# additional cleaning
-    #     print("Cleaning Infogroup")
-	#     clean.clean_infogroup(data_dir)
-    # cleaning CAFO permit data
-    # clean.clean_CAFO()
-	# except Exception as e:
-	# 	# maybe some sort of logging - could have a logs folder where this saves error messages
-	# 	# short term solution
-	# 	print(f"Oops you screwed up dummy: {e}")
-	# 	exit(1)
+def main(token): # what should we put in datadir? which ones should take user input? 
 	try:
 		# Data Cleaning
 		print("Cleaning FSIS data...")
-		# clean.clean_FSIS()
+		clean.clean_FSIS("../data/raw/fsis-processors-with-location.csv")
 		print("Cleaning Counterglow data...")
-		# clean.clean_counterglow()
+		clean.clean_counterglow("../data/raw/Counterglow+Facility+List+Complete.csv")
 		print("Cleaning Infogroup data...")
-		# clean.clean_infogroup()
+		clean.clean_infogroup()
 		print("Cleaning CAFO Permit data...")
-    except Exception as e:
-        print("test")
+		clean.clean_CAFO("../data/raw/cafo")
+
+		# Match plants and farms
+		print("Matching FSIS and Infogroup...")
+		match_plants.save_all_matches("../data/clean/poultry_plants_2022.csv",\
+				"../data/cleaned_fsis_processors.csv", 5)
+		print("Matching CAFO permit data and Counterglow for poultry plants...")
+		match_farms.match_all_farms("../data/raw/Counterglow+Facility+List+Complete.csv",\
+			      "../data/clean/cleaned_matched_farms.csv", "Poultry|Chicken|Broiler")
+		
+		# Generate GeoJSONs and maps
+		print("Creating plant capture GeoJSON...")
+		calculate_captured_areas.full_script(token)
+		
+		print("Mapping CAFO permits...")
+		match_df = pd.read_csv("../data/clean/matched_farms.csv")
+		states = match_df["state"].unique().tolist()
+		for state in states:
+			path = "../html/cafo_poultry_eda_" + "state" + ".html"
+			visualize.map_state("../data/clean/matched_farms.csv", "../data/clean/unmatched_farms.csv", state).save(path)
+
+	except:
+	# except Exception as e:
+	# 	maybe some sort of logging - could have a logs folder where this saves error messages
+	# 	short term solution
+	# 	print(f"Oops you screwed up dummy: {e}")
+	# 	exit(1)
+		exit(1)
 
 if __name__ is "__main__":
-	main("../data/raw")
+	main()
