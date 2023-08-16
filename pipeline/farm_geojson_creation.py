95508aa9735742e84fa1a299dab53879585baf80
Alena Zeng <azeng@cs.uchicago.edu>
2023-08-16 15:56:26 -0500

    formatting

--- pipeline/farm_geojson_creation.py
+++ pipeline/farm_geojson_creation.py
@@ -9,26 +9,32 @@ from constants import abb2state, COUNTERGLOW_GEOJSON_FPATH
 
 def create_counterglow_geojson(cg_path: str, 
                                 states_geojson_path: str) -> None:
-    """Filters the Counterglow dataset for only poultry and generates Counterglow GeoJSON
-    with plant access data from all_states_with_parent_corporation_by_corp.geojson. 
-
+    """Filters the Counterglow dataset for only poultry 
+    and generates Counterglow GeoJSON based on plant access data 
+    from all_states_with_parent_corporation_by_corp.geojson, 
+    the file output by calculate_captured_areas.py
+    
     Args:
-        cg_path: relative path to the clean data folder with the Counterglow dataset.
-        states_geojson_path: relative path to all_states_with_parent_corporation_by_corp.geojson. 
+        cg_path: relative path to the clean data folder with the 
+            Counterglow dataset.
+        states_geojson_path: relative path to 
+            all_states_with_parent_corporation_by_corp.geojson. 
 
     Returns:
         N/A, writes cleaned Counterglow GeoJSON to the clean data folder.
     
     """
     df = pd.read_csv(cg_path)
-    df_poultry = df[(df["Farm Type"] == "Chickens (Meat)") | (df["Farm Type"] == "Chickens & Other Birds (Meat)")]
+    df_poultry = df[(df["Farm Type"] == "Chickens (Meat)") | 
+                    (df["Farm Type"] == "Chickens & Other Birds (Meat)")]
     df_states = gpd.read_file(states_geojson_path)
 
     list_of_farms = []
     for state in df_poultry.State.unique():
         for _, farm in df_poultry[df_poultry.State == state].iterrows():
             pt = Point(farm["Longitude"], farm["Latitude"])
-            for _, area in df_states[df_states.state == abb2state[state]].iterrows():
+            for _, area in df_states[df_states.state == abb2state[state]
+                                     ].iterrows():
                 if area.geometry.contains(pt):
                     farm_data = {
                         "state": abb2state[state],
