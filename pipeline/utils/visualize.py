f2c94cf46ad5ce790ade8644fa0348a4c13f9b41
toddnief <todd.nief@gmail.com>
2023-08-17 09:48:03 -0500

    pipeline package may work
    Former-commit-id: 792cd8804aa183ca9457059870045f28b7241d87

--- pipeline/utils/visualize.py
+++ pipeline/utils/visualize.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 import folium
 from pathlib import Path
-from ..constants import MATCHED_FARMS_FPATH, UNMATCHED_FARMS_FPATH, DATA_DIR
+from pipeline.constants import MATCHED_FARMS_FPATH, UNMATCHED_FARMS_FPATH, DATA_DIR
 
 
 def colored_maps(infogroup_path: Path, year: int):
@@ -14,29 +14,49 @@ def colored_maps(infogroup_path: Path, year: int):
     USA_LNG = -95.7129
 
     # assigning each parent corp a unique color
-    dict_place = dict({"Tyson Foods Inc": 'green', 
-                "JBS USA": 'red', 
-                "Sanderson Farms Inc": 'pink', 
-                "Hormel Foods Corp": 'cadetblue', 
-                "Koch Foods Inc": 'beige'})
-    
+    dict_place = dict(
+        {
+            "Tyson Foods Inc": "green",
+            "JBS USA": "red",
+            "Sanderson Farms Inc": "pink",
+            "Hormel Foods Corp": "cadetblue",
+            "Koch Foods Inc": "beige",
+        }
+    )
+
     # coloring all other parent corporations gray
-    placeholder = ["None", "Pilgrim's Pride Corp", "Cargil Inc", "Mountaire Corp", "Foster Farms", "Perdue Farms Inc", "Continental Grain Co", 
-               "George's Inc", "House of Raeford Farms Inc", "Cal-main Foods Inc", "Conagra Brands Inc", "UNKNOWN", "Simmons Foods Inc",
-                "Peco Foods Inc"]
+    placeholder = [
+        "None",
+        "Pilgrim's Pride Corp",
+        "Cargil Inc",
+        "Mountaire Corp",
+        "Foster Farms",
+        "Perdue Farms Inc",
+        "Continental Grain Co",
+        "George's Inc",
+        "House of Raeford Farms Inc",
+        "Cal-main Foods Inc",
+        "Conagra Brands Inc",
+        "UNKNOWN",
+        "Simmons Foods Inc",
+        "Peco Foods Inc",
+    ]
     dict_place2 = dict.fromkeys(placeholder, "lightgray")
 
     col_dict = {**dict_place, **dict_place2}
 
     master = pd.read_csv(infogroup_path, index=False).reset_index(drop=True)
-    df = master.loc[master['ARCHIVE VERSION YEAR']==year]
-    map_name = folium.Map(location=[USA_LAT, USA_LNG],zoom_start=4)
-    df['COLOR'] = df['PARENT NAME'].map(col_dict)
+    df = master.loc[master["ARCHIVE VERSION YEAR"] == year]
+    map_name = folium.Map(location=[USA_LAT, USA_LNG], zoom_start=4)
+    df["COLOR"] = df["PARENT NAME"].map(col_dict)
 
     for _, location_info in df.iterrows():
-        folium.Marker([location_info["LATITUDE"], location_info["LONGITUDE"]], popup = location_info['PARENT NAME'],
-                icon=folium.Icon(color=location_info['COLOR'])).add_to(map_name)
-        
+        folium.Marker(
+            [location_info["LATITUDE"], location_info["LONGITUDE"]],
+            popup=location_info["PARENT NAME"],
+            icon=folium.Icon(color=location_info["COLOR"]),
+        ).add_to(map_name)
+
     return map_name
 
 
@@ -53,8 +73,11 @@ def add_points(state_map: folium.Map, state_df: pd.DataFrame, color: str):
 
     """
     for index, location_info in state_df.iterrows():
-        folium.Marker([location_info["lat"], location_info["long"]],\
-            popup=location_info["name"], icon=folium.Icon(color=color)).add_to(state_map)
+        folium.Marker(
+            [location_info["lat"], location_info["long"]],
+            popup=location_info["name"],
+            icon=folium.Icon(color=color),
+        ).add_to(state_map)
 
 
 def map_state(match_df_path: Path, unmatched_df_path: Path, state: str):
@@ -71,16 +94,24 @@ def map_state(match_df_path: Path, unmatched_df_path: Path, state: str):
     """
     match_df = pd.read_csv(match_df_path)
     unmatched_df = pd.read_csv(unmatched_df_path)
-    state_map = folium.Map(location=[match_df.lat.mean(), match_df.long.mean()], zoom_start=7, control_scale=True)
+    state_map = folium.Map(
+        location=[match_df.lat.mean(), match_df.long.mean()],
+        zoom_start=7,
+        control_scale=True,
+    )
 
-    match_state = match_df[match_df["state"]==state]
-    unmatched_state = unmatched_df[unmatched_df["state"]==state]
-    
-    add_points(state_map, match_state, "green") # adding points that matched on both
-    add_points(state_map, unmatched_state[unmatched_state["source"]!="Counterglow"], "blue") # adding points unique to permit dataset
-    
-    cg_unique = unmatched_df[unmatched_df["source"]=="Counterglow"]
-    add_points(state_map, cg_unique[cg_unique["state"]==state], "red") # adding points unique to Counterglow 
+    match_state = match_df[match_df["state"] == state]
+    unmatched_state = unmatched_df[unmatched_df["state"] == state]
+
+    add_points(state_map, match_state, "green")  # adding points that matched on both
+    add_points(
+        state_map, unmatched_state[unmatched_state["source"] != "Counterglow"], "blue"
+    )  # adding points unique to permit dataset
+
+    cg_unique = unmatched_df[unmatched_df["source"] == "Counterglow"]
+    add_points(
+        state_map, cg_unique[cg_unique["state"] == state], "red"
+    )  # adding points unique to Counterglow
 
     return state_map
 
@@ -91,4 +122,6 @@ if __name__ == "__main__":
 
     for state in states:
         path = "html/cafo_poultry_eda_" + state + ".html"
-        map_state(MATCHED_FARMS_FPATH, UNMATCHED_FARMS_FPATH, state).save(DATA_DIR / path)
+        map_state(MATCHED_FARMS_FPATH, UNMATCHED_FARMS_FPATH, state).save(
+            DATA_DIR / path
+        )
