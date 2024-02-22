deb87e88222060e244bec660137728a12a6139b7
toddnief <todd.nief@gmail.com>
2024-02-22 09:24:13 -0600

    don't ignore lib pls
    Former-commit-id: 0ca7e1b8b88dcf31b8c75976903158d717cf28c6

---
+++ dashboard/lib/data.js
@@ -0,0 +1,75 @@
+"use client";
+import Papa from "papaparse";
+import { coords2geo } from "geotoolbox";
+
+import { state, updateFilteredData } from "../lib/state";
+
+// const POULTRY_PLANTS_CSV = "../data/poultry_plants_with_sales.csv";
+const POULTRY_PLANTS_CSV = "../data/location_match_fullest.csv";
+// const PLANT_ACCESS_GEOJSON = "../data/all_states.geojson";
+const PLANT_ACCESS_GEOJSON =
+  "../data/new_all_states_with_parent_corp_by_corp.geojson";
+// const COUNTERGLOW_FARMS = "../data/counterglow_geojson.geojson";
+const FARMS = "../data/nc_farms_plants.geojson";
+
+const getJSON = async (dataPath) => {
+  const response = await fetch(dataPath);
+  return await response.json();
+};
+
+const getPoultryCSV = async (dataPath) => {
+  const response = await fetch(dataPath);
+  const reader = response.body.getReader();
+  const result = await reader.read(); // raw array
+  const decoder = new TextDecoder("utf-8");
+  const csv = decoder.decode(result.value);
+
+  return new Promise((resolve, reject) => {
+    Papa.parse(csv, {
+      header: true,
+      complete: (results) => {
+        const data = results.data.map((row) => ({
+          ...row,
+          "Sales Volume (Location)": parseFloat(row["Sales Volume (Location)"]),
+        }));
+
+        resolve(data);
+      },
+      error: (error) => {
+        reject(error);
+      },
+    });
+  });
+};
+
+// Function to load data
+export const loadData = async () => {
+  // Read raw files
+  state.stateData.plantAccess = await getJSON(PLANT_ACCESS_GEOJSON);
+  state.stateData.allStates = state.stateData.plantAccess.features
+    .map((feature) => feature.properties.state)
+    .filter((value, index, array) => array.indexOf(value) === index)
+    .sort();
+  // TODO: Switch variable name to be non-specific to counterglow
+  state.stateData.counterglowFarms = await getJSON(FARMS);
+  state.stateData.farms = await getJSON(FARMS);
+
+  // Filter FSIS plant data
+  const rawPlants = await getPoultryCSV(POULTRY_PLANTS_CSV);
+  const rawPoultryPlants = rawPlants.filter((row) => {
+    if (row["Animals Processed"] === "Chicken" && row.Size === "Large") {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  state.stateData.poultryPlants = coords2geo(rawPoultryPlants, {
+    lat: "latitude",
+    lng: "longitude",
+  });
+
+  // Initialize display data
+  state.stateData.filteredStates = [...state.stateData.allStates]; // Start with all states selected
+  updateFilteredData();
+  state.stateData.isDataLoaded = true;
+};
---
+++ dashboard/lib/state.js
@@ -0,0 +1,226 @@
+"use client";
+import { proxy, useSnapshot } from "valtio";
+import bbox from "@turf/bbox";
+import { WebMercatorViewport } from "@deck.gl/core";
+import booleanWithin from "@turf/boolean-point-in-polygon";
+
+// Create a proxy state
+export const state = proxy({
+  // basic data
+  stateData: {
+    counterglowFarms: [],
+    plantAccess: [],
+    poultryPlants: [],
+    allStates: [],
+
+    // filtered data
+    filteredStates: [],
+    filteredPlants: [],
+    filteredCaptureAreas: [],
+    filteredCompanies: [],
+    filteredSales: [],
+    capturedAreas: {},
+    isDataLoaded: false,
+  },
+
+  //TODO: Unsure about the best practices for how to load and manage these nested states
+  stateMapSettings: {
+    // cursor state
+    x: undefined,
+    y: undefined,
+    hoveredObject: undefined,
+
+    // display options
+    displayFarms: false,
+
+    // map view
+    containerWidth: 0,
+    containerHeight: 0,
+    mapZoom: undefined,
+  },
+});
+
+function updateFilteredStates() {
+  // choose the filtered areas to display
+  state.stateData.filteredCaptureAreas =
+    state.stateData.plantAccess.features.filter((row) => {
+      if (state.stateData.filteredStates.includes(row.properties.state)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+}
+
+function updateFilteredPlants() {
+  state.stateData.filteredPlants =
+    state.stateData.poultryPlants.features.filter((row) => {
+      if (state.stateData.filteredStates.includes(row.properties.State)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+}
+
+function updateFilteredCompanies() {
+  state.stateData.filteredCompanies = state.stateData.filteredPlants
+    .map((plant) => plant.properties["Parent Corporation"])
+    .filter((value, index, array) => array.indexOf(value) === index);
+}
+
+function updateFilteredSales() {
+  // build dictionary for each company in the area
+  let companySales = {};
+  for (let i = 0; i < state.stateData.filteredCompanies.length; i++) {
+    companySales[state.stateData.filteredCompanies[i]] = 0;
+  }
+
+  for (let i = 0; i < state.stateData.filteredPlants.length; i++) {
+    let salesVolume =
+      state.stateData.filteredPlants[i].properties["Sales Volume (Location)"];
+    if (!Number.isNaN(salesVolume)) {
+      companySales[
+        state.stateData.filteredPlants[i].properties["Parent Corporation"]
+      ] += salesVolume;
+    }
+  }
+
+  // filter NaN values and return dictionary
+  let filtered = Object.entries(companySales).reduce(
+    (filtered, [key, value]) => {
+      if (!Number.isNaN(value)) {
+        filtered[key] = value;
+      }
+      return filtered;
+    },
+    {}
+  );
+
+  // sort on value and convert to object
+  let sorted = Object.entries(filtered).sort((a, b) => b[1] - a[1]);
+  let unnestedSales = Object.fromEntries(sorted);
+
+  const totalSales = Object.values(unnestedSales).reduce(
+    (accumulator, value) => {
+      return accumulator + value;
+    },
+    0
+  );
+
+  // create nested object for each corporation
+  let nestedSales = {};
+  for (let key in unnestedSales) {
+    nestedSales[key] = {
+      sales: unnestedSales[key],
+      percent: unnestedSales[key] / totalSales,
+    };
+  }
+  state.stateData.filteredSales = nestedSales;
+}
+
+function updateMapZoom() {
+  // default zoom state is everything (handles the case of no selection)
+  var zoomGeoJSON = state.stateData.plantAccess.features;
+
+  // update to the selected areas if they exist
+  if (state.stateData.filteredStates.length) {
+    zoomGeoJSON = state.stateData.filteredCaptureAreas;
+  }
+
+  const currentGeojson = {
+    type: "FeatureCollection",
+    features: zoomGeoJSON,
+  };
+  const boundingBox = bbox(currentGeojson);
+  const fittedViewport = new WebMercatorViewport(
+    state.stateMapSettings.containerWidth,
+    state.stateMapSettings.containerHeight
+  );
+
+  const currentLatLonZoom = fittedViewport.fitBounds(
+    [
+      [boundingBox[0], boundingBox[1]],
+      [boundingBox[2], boundingBox[3]],
+    ],
+    {
+      width: state.stateMapSettings.containerWidth,
+      height: state.stateMapSettings.containerHeight,
+      padding: { top: 20, bottom: 20, left: 20, right: 20 },
+    }
+  );
+
+  state.stateMapSettings.mapZoom = {
+    longitude: currentLatLonZoom.longitude,
+    latitude: currentLatLonZoom.latitude,
+    zoom: currentLatLonZoom.zoom,
+
+    pitch: 0,
+    bearing: 0,
+  };
+}
+
+function calculateCapturedArea() {
+  let areas = {
+    1: 0,
+    2: 0,
+    3: 0,
+    // 4: 0,
+  };
+
+  // TODO: Need to add area to GeoJSON
+  for (let i = 0; i < state.stateData.filteredCaptureAreas.length; i++) {
+    areas[
+      state.stateData.filteredCaptureAreas[i].properties.corporate_access
+    ] += state.stateData.filteredCaptureAreas[i].properties.area;
+  }
+
+  let totalArea = Object.values(areas).reduce((acc, val) => acc + val, 0);
+
+  let percentArea = {};
+  Object.keys(areas).forEach((key) => {
+    percentArea[key] = areas[key] / totalArea;
+  });
+
+  // return percentArea;
+  state.stateData.capturedAreas = percentArea;
+}
+
+function calculateCapturedAreaByBarns() {
+  // initialize object for reduce operation
+  const counts = {
+    totalFarms: 0,
+    plantAccessCounts: {
+      0: 0, // '0' represents NaN or no access
+      1: 0,
+      2: 0,
+      3: 0,
+    },
+  };
+
+  state.stateData.farms.features.reduce((accumulator, feature) => {
+    accumulator.totalFarms += 1;
+
+    const plantAccess = feature.properties.plant_access || "0"; // Default to '0' if null
+    accumulator.plantAccessCounts[plantAccess] += 1;
+
+    return accumulator;
+  }, counts);
+
+  let percentCaptured = {};
+  Object.keys(counts.plantAccessCounts).forEach((key) => {
+    percentCaptured[key] = counts.plantAccessCounts[key] / counts.totalFarms;
+  });
+
+  state.stateData.capturedAreas = percentCaptured;
+}
+
+export function updateFilteredData() {
+  updateFilteredStates();
+  updateFilteredPlants();
+  updateFilteredCompanies();
+  updateFilteredSales();
+  calculateCapturedArea();
+  calculateCapturedAreaByBarns();
+  updateMapZoom();
+}
