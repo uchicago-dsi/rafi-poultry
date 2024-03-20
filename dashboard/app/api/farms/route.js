c41101fc22c00a12a5bd9ca863e246f638e5e317
toddnief <todd.nief@gmail.com>
2024-03-20 13:29:05 -0500

    update plants to come from API
    Former-commit-id: 738449f231c1163b262ce101f050464951b213d5

--- dashboard/app/api/farms/route.js
+++ dashboard/app/api/farms/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import path from "path";
 import fs from 'fs';
 
-const FARMS = "test_barns_filtering.geojson";
+const FARMS = "test_barns.geojson";
 
 export async function GET(_req) {
   const filePath = path.join(process.cwd(), 'public', 'data', FARMS);
