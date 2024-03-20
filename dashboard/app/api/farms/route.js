8532b631a77b73c522e95bb71cd62632de2c042f
toddnief <todd.nief@gmail.com>
2024-03-20 18:24:58 -0500

    loading from cloud storage works
    Former-commit-id: e8b01143e8b2b0bf9969c4ff78c5d34c524050ea

--- dashboard/app/api/farms/route.js
+++ dashboard/app/api/farms/route.js
@@ -1,12 +1,24 @@
+import { Storage } from '@google-cloud/storage';
 import { NextResponse } from "next/server";
 import path from "path";
 import fs from 'fs';
 
 const FARMS = "test_barns.geojson";
 
+// TODO: This is the version for loading locally
+// export async function GET(_req) {
+//   const filePath = path.join(process.cwd(), 'public', 'data', FARMS);
+//   const fileContents = fs.readFileSync(filePath);
+//   const data = JSON.parse(fileContents);
+//   return NextResponse.json(data, { status: 200 });
+// }
+
 export async function GET(_req) {
-  const filePath = path.join(process.cwd(), 'public', 'data', FARMS);
-  const fileContents = fs.readFileSync(filePath);
-  const data = JSON.parse(fileContents);
-  return NextResponse.json(data, { status: 200 });
-}
+    const storage = new Storage();
+    const bucket = storage.bucket(bucketName);
+    const file = bucket.file(FARMS);
+    console.log(file)
+    const [fileContents] = await file.download();
+    const data = JSON.parse(fileContents.toString('utf-8'));
+    return NextResponse.json(data, { status: 200 });
+  }
--- dashboard/app/api/plants/route.js
+++ dashboard/app/api/plants/route.js
@@ -1,12 +1,27 @@
+import { Storage } from '@google-cloud/storage';
 import { NextResponse } from "next/server";
 import path from "path";
 import fs from 'fs';
 
+// const PLANTS = "test_plants.geojson";
+
+// TODO: This is the version for loading the files locally
+// export async function GET(_req) {
+//     const filePath = path.join(process.cwd(), 'public', 'data', PLANTS);
+//     const fileContents = fs.readFileSync(filePath);
+//     const data = JSON.parse(fileContents);
+//     return NextResponse.json(data, { status: 200 });
+//   }
+
 const PLANTS = "test_plants.geojson";
+const bucketName = 'rafi-poultry';
 
 export async function GET(_req) {
-    const filePath = path.join(process.cwd(), 'public', 'data', PLANTS);
-    const fileContents = fs.readFileSync(filePath);
-    const data = JSON.parse(fileContents);
-    return NextResponse.json(data, { status: 200 });
-  }
+  const storage = new Storage();
+  const bucket = storage.bucket(bucketName);
+  const file = bucket.file(PLANTS);
+  console.log(file)
+  const [fileContents] = await file.download();
+  const data = JSON.parse(fileContents.toString('utf-8'));
+  return NextResponse.json(data, { status: 200 });
+}
