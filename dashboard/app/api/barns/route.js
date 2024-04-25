10c37a9db1a37f0679f7f11f999c3808b2d1fd5f
toddnief <todd.nief@gmail.com>
2024-04-25 16:47:43 -0500

    stream msgpack from google cloud

--- dashboard/app/api/barns/route.js
+++ dashboard/app/api/barns/route.js
@@ -24,8 +24,24 @@ export async function GET(_req) {
       });
     const bucket = storage.bucket(bucketName);
     const file = bucket.file(BARNS);
-    const [fileContents] = await file.download();
-    const data = unpack(fileContents);
-    return NextResponse.json(data, { status: 200 });
+    // const [fileContents] = await file.download();
+    // const data = unpack(fileContents);
+    // return NextResponse.json(data, { status: 200 });
+    const readStream = file.createReadStream();
+    const chunks = []; // This array will hold our data chunks
+
+    return new Promise((resolve, reject) => {
+      readStream.on('data', (chunk) => {
+        chunks.push(chunk); // Collect data chunks in an array
+      }).on('end', () => {
+        // Combine all the chunks and unpack the data
+        const combined = Buffer.concat(chunks);
+        const data = unpack(combined); // Unpack data after all chunks are received
+        resolve(NextResponse.json(data, { status: 200 }));
+      }).on('error', (error) => {
+        console.error('Error downloading the file', error);
+        reject(NextResponse.json({ error: 'Failed to download file' }, { status: 500 }));
+      });
+    });
   }
 }
