d12e2b1debb72bb1b7be37ce2cc205c1aee6e053
toddnief <todd.nief@gmail.com>
2024-04-25 16:13:11 -0500

    switch to loading barns as msgpack

--- dashboard/app/api/barns/route.js
+++ dashboard/app/api/barns/route.js
@@ -2,10 +2,10 @@ import { Storage } from '@google-cloud/storage';
 import fs from 'fs';
 import { NextResponse } from "next/server";
 import path from "path";
+import { unpack } from "msgpackr";
 
 const bucketName = 'rafi-poultry';
-// const BARNS = "filtered_barns.geojson";
-const BARNS = "filtered_barns_NC_NE_AR_GA_SC_LA_MS_DE_MD_VA_WV.geojson";
+const BARNS = "all_barns.msgpack";
 const serviceAccountKey = JSON.parse(Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64, 'base64').toString('ascii'));
 
 const DATA_SOURCE = "cloud" // "local" or "cloud"
@@ -14,7 +14,8 @@ export async function GET(_req) {
   if (DATA_SOURCE == "local") {
     const filePath = path.join(process.cwd(), 'public', 'data', BARNS);
     const fileContents = fs.readFileSync(filePath);
-    const data = JSON.parse(fileContents);
+    const data = unpack(fileContents);
+    console.log(data)
     return NextResponse.json(data, { status: 200 });
   }
   else {
@@ -23,9 +24,8 @@ export async function GET(_req) {
       });
     const bucket = storage.bucket(bucketName);
     const file = bucket.file(BARNS);
-    console.log(file)
     const [fileContents] = await file.download();
-    const data = JSON.parse(fileContents.toString('utf-8'));
+    const data = unpack(fileContents);
     return NextResponse.json(data, { status: 200 });
   }
 }
