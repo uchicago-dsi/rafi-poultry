e719dc917d9a3139e4fff4f1d1f9f6d077216475
toddnief <todd.nief@gmail.com>
2024-08-12 15:25:12 -0500

    fix flyto behavior and map state
    Former-commit-id: bf8bda0dbebb363517d5df1c332e8c77c72b95fb

--- dashboard/components/SummaryStats.js
+++ dashboard/components/SummaryStats.js
@@ -11,7 +11,7 @@ export function SummaryStats() {
   }
 
   return (
-    <div className="overflow-y-scroll overflow-x-hidden flex flex-row pb-2 mb-2">
+    <div className="overflow-y-scroll overflow-x-hidden flex flex-row pb-2 mb-2 bg-white">
       {filteredSales && Object.keys(filteredSales).length > 0 ? (
         <div className="p-2 w-[342px]">
           {filteredBarns && Object.keys(filteredBarns).length > 0 ? (
