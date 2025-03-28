6421b1c9363fc507ee58ce3e7091b416c93f80e6
nofurtherinformation <dhalpern@uchicago.edu>
2025-03-28 15:21:33 -0500

    add analytics

--- dashboard/app/layout.js
+++ dashboard/app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
+      <head>
+        <script defer src="https://core-facility-umami.vercel.app/script.js" data-website-id="247afa6c-d983-442f-a866-595409c05917"></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
